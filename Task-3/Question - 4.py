b9f9df291cc492faea7cce88ae179e02b5e9371e
Raagaveda <75232496+Raagaveda@users.noreply.github.com>
2022-06-18 21:19:25 +0530

    Update Question - 4.py

--- Task-3/Question - 4.py
+++ Task-3/Question - 4.py
@@ -1,7 +1,8 @@
 import pandas as pd
-ser = pd.Series(['amrita', 'school', 'of', 'engineering' ,'chennai' , 'campus'])
-print("Input series =", ser)
-newSeries = ser.str.title()  #series.map(lambda x: x[0].upper() + x[1:-1] + x[-1])
-print("\nResulting Series :",newSeries)
-
+ser = pd.Series(['amrita', 'school', 'of', 'engineering', 'chennai' , 'campus'])
+print("original series:\n")
+print(' '.join(ser))
+Series = ser.map(lambda x: x[0].upper() + x[1:-1] + x[-1].lower())
+print("\nResulting Series :\n")
+print(' '.join(Series))
                        
